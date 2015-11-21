7f234427c58de9755602dfe33f3c81554cf3ae6b
stivenrgalvis <jsrgalvis@gmail.com>
2015-11-21 18:54:49 -0400

    integra la funcionalidad de crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,21 @@
 class ActorsController < ApplicationController
 
-def index
-@actors=Actor.all
-end
+  def index
+    @actors=Actor.all
+  end
+
+  def create
+    @actor = Actor.new (actor_params)
+    if @actor.save
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
+
+    def new
+      @actor=Actor.new
+
+    end
 
 end
